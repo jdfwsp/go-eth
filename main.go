682295bbbe15d47c5b59aec6270b2c4dc0a9be5c
jdfwsp <jdfwsp@gmail.com>
2022-03-09 13:03:55 -0600

    Validate address

--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import (
 	"log"
 	"math"
 	"math/big"
+	"regexp"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func main() {
+	// establish connection to blockchain
 	client, err := ethclient.Dial("http://localhost:8545")
 	if err != nil {
 		log.Fatal(err)
@@ -19,6 +21,7 @@ func main() {
 
 	fmt.Println("we have a connection")
 
+	// retrieve balance from a specified account
 	address := common.HexToAddress("0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1")
 	balance, err := client.BalanceAt(context.Background(), address, nil)
 	if err != nil {
@@ -27,9 +30,37 @@ func main() {
 
 	fmt.Println(balance)
 
+	// convert wei to eth
 	fbalance := new(big.Float)
 	fbalance.SetString(balance.String())
 	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
 
 	fmt.Println(ethValue)
+
+	// check validity of address
+	re := regexp.MustCompile("^0x[0-9a-fA-F]{40}$")
+
+	fmt.Printf("is valid: %v\n", re.MatchString("0x323b5d4c32345ced77393b3530b1eed0f346429d"))
+	fmt.Printf("is valid: %v\n", re.MatchString("0x323b5x4c32345cedz7393b3530b1eed0f346429d"))
+
+	address = common.HexToAddress("0xe41d2489571d322189246dafa5ebde1f4699f498")
+	bytecode, err := client.CodeAt(context.Background(), address, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	isContract := len(bytecode) > 0
+
+	fmt.Printf("is contract: %v\n", isContract)
+
+	address = common.HexToAddress("0x8e215d06ea7ec1fdb4fc5fd21768f4b34ee92ef4")
+	bytecode, err = client.CodeAt(context.Background(), address, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	isContract = len(bytecode) > 0
+
+	fmt.Printf("is contract: %v\n", isContract)
+
 }
